b85d019da7e8ac3f26bf9653b9d8fef7ed9757d4
CharannSdsConnect <sdsuser010@gmail.com>
2024-09-12 22:07:58 +0530

    Update AccountServiceImpl.java

--- src/main/java/net/java/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/net/java/banking/service/impl/AccountServiceImpl.java
@@ -5,11 +5,16 @@ import net.java.banking.entity.Account;
 import net.java.banking.mapper.AccountMapper;
 import net.java.banking.repository.AccountRepository;
 import net.java.banking.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    @Autowired
     private AccountRepository accountRepository;
 
     @Override
@@ -17,5 +22,74 @@ public class AccountServiceImpl implements AccountService {
         Account account = AccountMapper.mapToAccount(accountDto);
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto getAccountById(Long id) {
+
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(
+                ()-> new RuntimeException("Invalid id")
+        );
+
+        return AccountMapper.mapToAccountDto(account);
+    }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(
+                        ()-> new RuntimeException("Invalid id")
+                );
+        account.setBalance(
+                account.getBalance() + amount
+        );
+
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(
+                        ()-> new RuntimeException("Invalid id")
+                );
+        if(account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient amount");
+        }
+
+        account.setBalance(
+                account.getBalance() - amount
+        );
+
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List<Account> accounts = accountRepository.findAll();
+        return accounts.stream()
+                .map((account) ->
+                        AccountMapper.mapToAccountDto(account))
+                            .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(
+                        ()-> new RuntimeException("Invalid id")
+                );
+
+        accountRepository.deleteById(id);
     }
 }
